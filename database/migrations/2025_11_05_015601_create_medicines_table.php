9d8b5ffd04378cb04d3f82dadcec3a493164d3ae
DGXy24 <dolyboantua@gmail.com>
2025-11-19 08:51:21 +0700

    sedikit update

--- database/migrations/2025_11_05_015601_create_medicines_table.php
+++ database/migrations/2025_11_05_015601_create_medicines_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('medicines', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
-            $table->foreignId('supplier_id')->constrained('suppliers')->onDelete('set null')->nullable();
+            $table->foreignId('supplier_id')
+                ->nullable()
+                ->constrained('suppliers')
+                ->nullOnDelete();
             $table->string('name');
             $table->string('generic_name')->nullable();
             $table->text('description')->nullable();
